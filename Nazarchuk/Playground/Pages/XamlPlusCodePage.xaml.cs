55300d043ea8dd1e48602dba6807863e96de3fac
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-02-28 23:27:48 +0200

    Data binding and XAML animations

--- Nazarchuk/Playground/Pages/XamlPlusCodePage.xaml.cs
+++ Nazarchuk/Playground/Pages/XamlPlusCodePage.xaml.cs
@@ -6,7 +6,7 @@ namespace Playground
         {
             InitializeComponent();
         }
-
+        
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
             double slidervalue = args.NewValue * 100;
@@ -15,6 +15,15 @@ namespace Playground
         async void OnButtonClicked(object sender, EventArgs args)
         {
             Button button = (Button)sender;
+            await image.RotateTo(360, 2000);
+            image.Rotation = 0;
+            await image.ScaleTo(2, 2000);
+            await image.ScaleTo(1, 2000);
+            image.Opacity = 0;
+            await image.FadeTo(1, 4000);
+            double radius = Math.Min(absoluteLayout.Width, absoluteLayout.Height);
+            image.AnchorY = radius / image.Height;
+            await image.RotateTo(360, 2000);
             await DisplayAlert("Clicked", "The button labeled '" + button.Text + button.FontFamily + "' has been clicked", "OK");
         }
     }
