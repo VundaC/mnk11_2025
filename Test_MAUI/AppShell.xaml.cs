bd81f4f23c9d6c3ab89c3b390f48f73491497d11
Vitalii <vitaliy.doronin4@gmail.com>
2025-03-03 08:22:57 +0200

    Add Test project

---
+++ Test_MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Test_MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Test_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
