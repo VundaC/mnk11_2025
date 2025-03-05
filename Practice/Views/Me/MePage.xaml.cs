f57f76ecd7a115a0e3642a3466ce34def646121e
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-03-05 13:37:05 +0200

    Image added

--- Practice/Views/Me/MePage.xaml.cs
+++ Practice/Views/Me/MePage.xaml.cs
@@ -5,7 +5,19 @@
         public MePage()
         {
             InitializeComponent();
-            BindingContext = new MePageViewModel(); 
+            BindingContext = new MePageViewModel();
+            Image image = new Image
+            {
+                Source = ImageSource.FromFile("dotnet_bot.png")
+
+            };
+            using var memoryStream = new MemoryStream();
+            //image.Source.CopyTo(memoryStream);
+
+            base64EncodedImage.Text = Convert.ToBase64String(memoryStream.ToArray());
+
+            var imageBytes = Convert.FromBase64String(base64EncodedImage.Text);
+
         }
     }
 }
