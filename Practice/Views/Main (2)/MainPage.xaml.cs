035a48faecb62b5ef3419a79b1293c01627036b0
Dima Humennyi <strikazoid.of@gmail.com>
2025-03-16 13:07:40 +0200

    Main

---
+++ Practice/Views/Main (2)/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MauiApp.ViewModels.Main;
+
+namespace MauiApp.Views.Main;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainPageViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
