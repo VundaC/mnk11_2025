1b37709710e22b6b9035e6f2257def77b024fbff
Vitalii <vitaliy.doronin4@gmail.com>
2025-03-17 09:42:20 +0200

    Add changing themes

--- Practice/Views/Settings/SettingsPageViewModel.cs
+++ Practice/Views/Settings/SettingsPageViewModel.cs
@@ -5,18 +5,50 @@ namespace Practice.ViewModels;
 
 public class SettingsPageViewModel
 {
-    public ICommand AboutCommand { get; }
-    private readonly IDialogService _dialogService;
-    public string Version { get; } 
+    private const string ThemePreferenceKey = "AppTheme";
 
-    public SettingsPageViewModel(IDialogService dialogService, IPlatformService platformService)
+    public ICommand AboutCommand { get; }
+    public ICommand ToggleThemeCommand { get; }
+    private readonly IDialogService _dialogService;
+    private readonly IPreference _preferenceService;
+    public string Version { get; }
+    
+    private bool _isDarkTheme;
+    public bool IsDarkTheme
+    {
+        get => _isDarkTheme;
+        set
+        {
+            if (_isDarkTheme != value)
+            {
+                _isDarkTheme = value;
+                _preferenceService.Save(ThemePreferenceKey, value);
+                OnThemeChanged();
+            }
+        }
+    }
+
+    public SettingsPageViewModel(IDialogService dialogService, IPlatformService platformService, IPreference preferenceService)
     {
         _dialogService = dialogService;
+        _preferenceService = preferenceService;
         Version = $"Version: {platformService.AppVersion}";
+
+        _isDarkTheme = _preferenceService.Get(ThemePreferenceKey, false);
 
         AboutCommand = new Command(async () =>
         {
             await _dialogService.ShowAlertAsync("About", $"This is a practice app! {Version}");
         });
+
+        ToggleThemeCommand = new Command(() =>
+        {
+            IsDarkTheme = !IsDarkTheme;
+        });
+    }
+
+    private void OnThemeChanged()
+    {
+        Application.Current.UserAppTheme = _isDarkTheme ? AppTheme.Dark : AppTheme.Light;
     }
 }
