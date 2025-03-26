7a4cd7f298c6d0835392a246ec5710ae579a9c5f
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-03-26 23:16:29 +0200

    revert

--- Practice/Views/Me/MePage.xaml.cs
+++ Practice/Views/Me/MePage.xaml.cs
@@ -2,11 +2,10 @@
 {
     public partial class MePage : ContentPage
     {
-        private readonly MePageViewModel _viewModel;
         public MePage(MePageViewModel viewModel)
         {
             InitializeComponent();
-            BindingContext = _viewModel = viewModel;
+            BindingContext = viewModel;
         }
     }
 }
