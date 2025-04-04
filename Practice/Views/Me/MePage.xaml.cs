4a0d70f02449f5b7b88594c0981ecc80c7802d99
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-04-04 11:39:51 +0300

    Revert "Fix problems"
    This reverts commit 21f73fe2266ce68eb6d59e69219429bbe72eabe2.

--- Practice/Views/Me/MePage.xaml.cs
+++ Practice/Views/Me/MePage.xaml.cs
@@ -3,19 +3,10 @@
     public partial class MePage : ContentPage
     {
         private readonly MePageViewModel _viewModel;
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         public MePage(MePageViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = _viewModel = viewModel;
-            try
-            {
-                Logger.Info("Page loaded");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, "Goodbye cruel world");
-            }
         }
         protected override void OnAppearing()
         {
