88dd8a44198a29707e41f88be35f8d97d491ab48
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-04-27 23:01:59 +0300

    path

---
+++ Practice/Controls/Path.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice.Controls;
+
+internal class RoundCircleRect : IDrawable
+{
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        PathF path = new PathF();
+    
+        path.MoveTo(10, 0);            
+        path.LineTo(190, 0);
+        path.AddArc(190, 0, 200, 10, 90, 0, true);
+
+
+        //path.LineTo();
+
+
+        canvas.StrokeColor = Colors.Red;
+        canvas.StrokeSize = 5;
+        canvas.DrawPath(path);
+    }
+}
+
+
